008b8688221d2286b3c81ec38bf24633e18aec03
Tyler Thrailkill <tyler.b.thrailkill@gmail.com>
2019-04-10 18:25:42 -0600

    Initial commit of nebula-mit-license gradle plugin

---
+++ build.gradle.kts
@@ -0,0 +1,40 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.3.21"
+    `java-gradle-plugin`
+    java
+    id("com.gradle.plugin-publish") version "0.10.0"
+}
+
+group = "com.tylerthrailkill.gradle"
+version = "0.0.1"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+gradlePlugin {
+    plugins {
+        create("mitLicensePlugin") {
+            id = "tylerthrailkill.nebula-mit-license"
+            displayName = "nebula-mit-license"
+            description = "Adds the MIT License to the POM of a nebula publication to allow for publishing to Maven Central."
+            implementationClass = "com.tylerthrailkill.gradle.maven.license.MavenMitLicensePlugin"
+        }
+    }
+}
+
+pluginBundle {
+    website = "https://gi"
+    vcsUrl = ""
+    tags = listOf("nebula", "license", "mit", "maven central")
+}
---
+++ src/main/java/com/tylerthrailkill/gradle/maven/license/MavenMitLicensePlugin.groovy
@@ -0,0 +1,31 @@
+package com.tylerthrailkill.gradle.maven.license
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.api.publish.maven.MavenPublication
+
+class MavenMitLicensePlugin implements Plugin<Project> {
+    @Override
+    void apply(Project project) {
+        project.plugins.apply MavenBasePublishPlugin
+
+        project.publishing {
+            publications {
+                nebula(MavenPublication)
+                withType(MavenPublication) { publication ->
+                    configureLicense(publication)
+                }
+            }
+        }
+    }
+
+    def configureLicense(MavenPublication publication) {
+        publication.pom.licenses {
+            license {
+                name = 'The MIT License'
+                url = 'https://opensource.org/licenses/MIT'
+                distribution = 'repo'
+            }
+        }
+    }
+}
